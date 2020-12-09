77ee9c0a4d37b9d95917997f99bb880f586a25e1
MD Shahriar Rahi <afmssrahi@gmail.com>
2020-12-09 22:41:08 +0600

    Fix a bug. If Stop-Watch is runnig, I can't call start function.

--- app.js
+++ app.js
@@ -7,18 +7,23 @@ const second = document.getElementById('second');
 let min;
 let sec = 0;
 let timer = null;
+let watchIsRunning = false;
 
 function start() {
-    timer = setInterval(function() {
-        sec++;
-        let {min, s} = getTime(sec);
-
-        minute.textContent = (min < 10) ? ('0' + min) : min;
-        second.textContent = (s < 10) ? ('0' + s) : s;
-    }, 1000)
+    if (!watchIsRunning) {
+        watchIsRunning = true;
+        timer = setInterval(function() {
+            sec++;
+            let {min, s} = getTime(sec);
+    
+            minute.textContent = (min < 10) ? ('0' + min) : min;
+            second.textContent = (s < 10) ? ('0' + s) : s;
+        }, 1000)
+    }
 }
 
 function reset() {
+    watchIsRunning = false;
     clearInterval(timer);
     min = 0;
     sec= 0;
@@ -28,6 +33,7 @@ function reset() {
 }
 
 function pause() {
+    watchIsRunning = false;
     clearInterval(timer);
 }
 
