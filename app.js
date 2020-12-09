e24c4de4a7170c1a3d4405cc6a2f3743b3a0fea4
MD Shahriar Rahi <afmssrahi@gmail.com>
2020-12-09 21:06:11 +0600

    Created app.js file and link this file with index.html file. After that written some code in app.js file to make Dynamic this Stop-Watch. Also change the title of this website in index.html file

---
+++ app.js
@@ -0,0 +1,46 @@
+const stopWatchCounterStart = document.getElementById('start');
+const stopWatchCounterReset = document.getElementById('reset');
+const stopWatchCounterPause = document.getElementById('pause');
+const minute = document.getElementById('minute');
+const second = document.getElementById('second');
+
+let min;
+let sec = 0;
+let timer = null;
+
+function start() {
+    timer = setInterval(function() {
+        sec++;
+        let {min, s} = getTime(sec);
+
+        minute.textContent =  min;
+        second.textContent = s;
+    }, 1000)
+}
+
+function reset() {
+    clearInterval(timer);
+    min = 0;
+    sec= 0;
+
+    minute.textContent = '00';
+    second.textContent = '00';
+}
+
+function pause() {
+    clearInterval(timer);
+}
+
+function getTime(sec) {
+    min = parseInt(sec / 60);
+    let s = parseInt(sec % 60);
+
+    return {
+        min,
+        s
+    }
+}
+
+stopWatchCounterStart.addEventListener('click', start);
+stopWatchCounterReset.addEventListener('click', reset);
+stopWatchCounterPause.addEventListener('click', pause);
